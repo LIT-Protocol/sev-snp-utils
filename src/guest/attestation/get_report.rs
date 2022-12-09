fb3459aa6bdfb695e8c54fad2b252f8809ad5587
Howard Tam <howard.tam.95@gmail.com>
2022-12-09 14:16:53 -0800

    Rename struct

--- src/guest/attestation/get_report.rs
+++ src/guest/attestation/get_report.rs
@@ -13,14 +13,14 @@ use crate::error::Result as Result;
 use super::get_report_types::SNP_REPORT_MSG_RESP_RESERVED_BYTES;
 
 #[derive(Debug)]
-pub struct SNPReportMsgResp {
+pub struct GetAttestationReportMsgResp {
     pub status: u32,
     pub report_size: u32,
     pub reserved: [u8; SNP_REPORT_MSG_RESP_RESERVED_BYTES],
     pub attestation_report: AttestationReport,
 }
 
-impl SNPReportMsgResp {
+impl GetAttestationReportMsgResp {
     pub fn from_reader(mut rdr: impl Read + Seek) -> Result<Self> {
         let status = rdr.read_u32::<LittleEndian>()
             .map_err(error::map_io_err)?;
@@ -29,7 +29,7 @@ impl SNPReportMsgResp {
         let reserved = read_exact_to_bin_vec(&mut rdr, SNP_REPORT_MSG_RESP_RESERVED_BYTES)?;
         let attestation_report = AttestationReport::from_reader(rdr)?;
 
-        Ok(SNPReportMsgResp {
+        Ok(GetAttestationReportMsgResp {
             status,
             report_size,
             reserved: reserved.as_slice().try_into().map_err(error::map_conversion_err)?,
@@ -66,7 +66,7 @@ impl SNPAttestationReportGetter {
         debug!("Retrieved guest report: {:?}", snp_guest_request_ioctl);
         
         // Check that the report was successfully generated.
-        let snp_report_msg = SNPReportMsgResp::from_reader(Cursor::new(snp_guest_request_ioctl.resp_data.data))?;
+        let snp_report_msg = GetAttestationReportMsgResp::from_reader(Cursor::new(snp_guest_request_ioctl.resp_data.data))?;
         debug!("SNP Report Message: {:?}", snp_report_msg);
         if snp_report_msg.status != 0 {
             return Err(error::Error::new_msg(error::Kind::Io, Some(format!("Non-zero status code {:?} with the following firmware error {:?}", snp_report_msg.status, snp_guest_request_ioctl.fw_err))));
@@ -82,14 +82,14 @@ mod tests {
 
     use crate::guest::attestation::get_report::SNP_REPORT_MSG_RESP_RESERVED_BYTES;
 
-    use super::SNPReportMsgResp;
+    use super::GetAttestationReportMsgResp;
 
     const TEST_MSG_RESP_BIN: &str = "resources/test/snp_report_msg_resp.bin";
 
     #[test]
     fn test_snp_report_msg_resp_from_reader() {
         let file_data = fs::read(TEST_MSG_RESP_BIN).unwrap();
-        let snp_report_msg_resp = SNPReportMsgResp::from_reader(Cursor::new(file_data)).unwrap();
+        let snp_report_msg_resp = GetAttestationReportMsgResp::from_reader(Cursor::new(file_data)).unwrap();
         
         assert_eq!(snp_report_msg_resp.status, 0);
         assert_eq!(snp_report_msg_resp.report_size, 1184);
